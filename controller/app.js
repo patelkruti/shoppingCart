f6bd3fbd0697b4e255095ee04042c82bb9de14c9
patelkruti <patelkruti.cs@gmail.com>
2015-03-25 16:30:23 -0400

    Update app.js

--- controller/app.js
+++ controller/app.js
@@ -7,15 +7,15 @@ app.controller('IncludeFooterCtrl', function($scope) {
     $scope.fileUrl = 'view/footer.html';
 });
 app.config(function ($routeProvider, $locationProvider) {
-	$routeProvider.when('/shoppingCart/home', {
+	$routeProvider.when('/home', {
 		templateUrl : 'view/home.html',
 		controller : "homePageCtrl"
 	})
-	.when('/shoppingCart/aboutUs', {
+	.when('/aboutUs', {
 		templateUrl: 'view/aboutUs.html'
 	})
 	.otherwise({
-		redirectTo: '/shoppingCart/home'
+		redirectTo: '/home'
 	});
 	$locationProvider.html5Mode({
 		enabled : true,
